d69c636019f43192c8e122418dc0bd4005b8b204
ejsmit <15705051+ejsmit@users.noreply.github.com>
2021-01-26 22:47:35 +0000

    fix sign user certs

--- bin/sign-user-keys.sh
+++ bin/sign-user-keys.sh
@@ -3,6 +3,9 @@
 # exit when any command fails
 set -e
 
+if [ -z SSH_USER_PRINCIPAL ]; do
+	echo "using $(whoami) as principal"
+
 echo "Creating new user key:"
 ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519
 
@@ -14,7 +17,7 @@ ssh-keygen -s ~/.private/apps/ssh-ca/ca-key \
     -I "user:$(whoami)@$(hostname)" \
     -n "user-$(whoami)" \
     -V -5m:+365d \
-    -z +$serial \
+    -z $serial \
     ~/.ssh/id_ed25519.pub
 
 ((serial=serial+1))
